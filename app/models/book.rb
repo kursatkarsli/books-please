ba01b48426ffa345dd85a86f1214fe9ed16160ae
Michał Maćkowiak <michal.mackowiak@cs.put.poznan.pl>
2017-09-13 16:08:50 +0200

    Added description attribute to a book and an enum for medium

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,3 @@
 class Book < ApplicationRecord
+    enum medium: { paper_book: 0, kindle: 1, audiobook: 2}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912140026) do
+ActiveRecord::Schema.define(version: 20170913140434) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170912140026) do
     t.integer "medium"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
 end
