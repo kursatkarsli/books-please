9dbc6255cce6a42a450cbf2bf1fe5cf4e2158640
Michał Maćkowiak <michal.mackowiak@cs.put.poznan.pl>
2017-09-16 13:54:33 +0200

    Refactored providing and rendering favourite books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,16 +2,12 @@ class BooksController < ApplicationController
   before_action :set_book, only: [:show, :toggle_favourite]
   def index
     @books = Book.all
-    @fav = Book.includes(:interests).where(
-      interests: { user_id: 4, is_favourite: true }
-    )
+    @favs = Book.favourites(current_user)
   end
 
   def show; end
 
   def toggle_favourite
-    @interest = @book.interests.find_by user_id: 4
-    @interest ||= Interest.create book: @book, user_id: 4
     @interest.toggle :is_favourite
     @interest.save
 
@@ -22,7 +18,7 @@ class BooksController < ApplicationController
 
   def set_book
     @book = Book.friendly.find(params[:id])
-    @interest = @book.interests.find_by user_id: 4
-    @interest ||= Interest.new
+    @interest = @book.interests.find_by user: current_user
+    @interest ||= Interest.create book: @book, user: current_user
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,13 @@
 class Book < ApplicationRecord
   has_many :interests
-  
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 
   enum medium: { paper_book: 0, kindle: 1, audiobook: 2 }
+
+  def self.favourites(user)
+    includes(:interests)
+      .where(interests: { user: user, is_favourite: true })
+  end
 end
